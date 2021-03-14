485befb6b0d9c52597737cf769e112c5529c27ac
twolak88 <twolak88@gmail.com>
2021-03-14 21:13:16 +0100

    Connecting the Store to React

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
@@ -38,7 +38,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <CounterOutput value={this.state.counter} />
+        <CounterOutput value={this.props.ctr} />
         <CounterControl
           label='Increment'
           clicked={() => this.counterChangedHandler('inc')}
@@ -60,4 +60,10 @@ class Counter extends Component {
   }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
