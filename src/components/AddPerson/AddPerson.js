50480c558ba3f928c2dc472352b62f7395658742
twolak88 <twolak88@gmail.com>
2021-03-15 20:40:31 +0100

    initial project

---
+++ src/components/AddPerson/AddPerson.js
@@ -0,0 +1,9 @@
+import './AddPerson.css';
+
+const addPerson = (props) => (
+    <div className="AddPerson">
+        <button onClick={props.personAdded}>Add Person</button>
+    </div>
+);
+
+export default addPerson;
