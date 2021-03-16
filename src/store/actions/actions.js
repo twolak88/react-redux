08e3df525b06aeffcf7371b578bd2b130baeb8db
twolak88 <twolak88@gmail.com>
2021-03-16 22:21:46 +0100

    refactoring

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -8,35 +8,35 @@ export const  DELETE_RESULT = 'DELETE_RESULT';
 export const increment = () => {
   return {
     type: INCREMENT
-  }
+  };
 };
 
 export const decremet = () => {
   return {
     type: DECREMENT
-  }
+  };
 };
 
 export const add = (value) => {
   return {
     type: ADD,
     value: value
-  }
+  };
 };
 
 export const subtract = (value) => {
   return {
     type: SUBTRACT,
     value: value
-  }
+  };
 };
 
 export const saveResult = (result) => {
   return {
     type: STORE_RESULT,
     result: result
-  }
-}
+  };
+};
 
 export const storeResult = (result) => {
   return dispatch => {
@@ -50,5 +50,5 @@ export const deleteResult = (resElId) => {
   return {
     type: DELETE_RESULT,
     resElId: resElId
-  }
+  };
 };
